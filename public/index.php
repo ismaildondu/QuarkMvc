ff3cea915d92edfe8d9c8f9bf2183e9d083292f9
ismail <ismaildondu7@gmail.com>
2023-11-13 19:02:53 +0300

    Is more readable for the default application

--- public/index.php
+++ public/index.php
@@ -2,12 +2,14 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 use QuarkMvc\app\Quark;
 $quark = new Quark();
+
+$quark->routes->setRoute('/', 'HomeController',"get");
+$quark->routes->addMiddleware('/', 'DemoMiddleware',"get");
+
+
 $quark->routes->setRoute('register', 'RegisterController',"get");
 $quark->routes->setRoute('register', 'RegisterController',"post");
-$quark->routes->setRoute('home', 'HomeController',"get");
 $quark->routes->setRoute('commands', 'CommandsController',"get");
-
-$quark->routes->addMiddleware('home', 'DemoMiddleware',"get");
 $quark->routes->set404Route('Custom404Controller');
 
 $quark->run();
